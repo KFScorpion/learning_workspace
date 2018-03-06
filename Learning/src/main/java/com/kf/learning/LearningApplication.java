d2e6875e93d866111d19605d65d4e04bfa41ab14
KFScorpion <kaizi881120@163.com>
2018-03-06 08:34:44 +0800

    init test

--- Learning/src/main/java/com/kf/learning/LearningApplication.java
+++ Learning/src/main/java/com/kf/learning/LearningApplication.java
@@ -1,12 +1,12 @@
 package com.kf.learning;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearningApplication.class, args);
+//		SpringApplication.run(LearningApplication.class, args);
+		System.out.println("init");
 	}
 }
